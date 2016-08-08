6dcde686cbc75d9eaf08599112ccb94772f299a2
bridiver <github@brianjohnson.cc>
2016-08-08 16:55:47 -0700

    remove vagrant

--- tools/chef/cookbooks/setup/metadata.rb
+++ tools/chef/cookbooks/setup/metadata.rb
@@ -6,5 +6,4 @@ description      'Installs/Configures dependencies for building Brave'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "vagrant"
 depends "build-essential"
