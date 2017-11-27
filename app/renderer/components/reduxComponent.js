335708525d85f60689b0e4904f4df3e1e820a70a
Pete Miller <miller.pete+commit@gmail.com>
2017-11-26 22:06:10 -0800

    perf: when choosing 'Toggle react profiling', also record mergeProps 'wasted' time
    This option now also counts all time that is spent in all component mergeProps functions that results in no properties actually changing (and therefore no render). This is an important metric to improve since this is blocking the renderer window UI thread, and the time is likely spent re-computing functions even though the state input to those functions has not changed.

--- app/renderer/components/reduxComponent.js
+++ app/renderer/components/reduxComponent.js
@@ -1,8 +1,10 @@
+const ipc = require('electron').ipcRenderer
 const appStore = require('../../../js/stores/appStoreRenderer')
 const ImmutableComponent = require('./immutableComponent')
 const React = require('react')
 const windowStore = require('../../../js/stores/windowStore')
 const {isList, isSameHashCode} = require('../../common/state/immutableUtil')
+const messages = require('../../../js/constants/messages')
 
 const mergePropsImpl = (stateProps, ownProps) => {
   return Object.assign({}, stateProps, ownProps)
@@ -39,6 +41,31 @@ const didPropsChange = function (oldProps, newProps) {
   return false
 }
 
+let perfRunningInterval = null
+let mergePropsWasteMs = 0
+ipc.on(messages.DEBUG_REACT_PROFILE, (e, args) => {
+  if (!perfRunningInterval) {
+    let totalMergePropsWaste = 0
+
+    const logMergePropsWaste = function () {
+      const timeWasted = mergePropsWasteMs
+      if (timeWasted) {
+        mergePropsWasteMs = 0
+        totalMergePropsWaste += timeWasted
+        console.log(`wasted ${timeWasted}ms in the last 1 second performing mergeProps that did not change, now wasted a total of ${totalMergePropsWaste}ms`)
+      }
+    }
+    perfRunningInterval = setInterval(logMergePropsWaste, 1000)
+  } else {
+    window.clearInterval(perfRunningInterval)
+    perfRunningInterval = null
+    mergePropsWasteMs = null
+    // TODO: show count of component mergeProps state re-evaluations,
+    // in order to track how many components are subscribing to state,
+    // as a metric to strive to reduce, for performance
+  }
+})
+
 class ReduxComponent extends ImmutableComponent {
   constructor (componentType, mergeStateToProps, props) {
     super(props)
@@ -51,9 +78,15 @@ class ReduxComponent extends ImmutableComponent {
 
   checkForUpdates () {
     if (!this.dontCheck) {
+      const t0 = perfRunningInterval && window.performance.now()
       const newState = this.buildProps(this.props)
+      const t1 = perfRunningInterval && window.performance.now()
       if (didPropsChange(this.state, newState)) {
         this.setState(newState)
+      } else if (perfRunningInterval) {
+        // log, if requested
+        const timeTaken = t1 - t0
+        mergePropsWasteMs += timeTaken
       }
     }
   }
