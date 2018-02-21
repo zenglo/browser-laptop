54fdee47ee1a553531c5c8b51b72a7920a6c7b30
Pete Miller <miller.pete@gmail.com>
2018-02-20 17:59:52 -0800

    no update events firing for unattached tabs, so manually fire on 'set-active' so we can at least activate tab and attach

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -638,6 +638,7 @@ const api = {
       })
 
       tab.on('set-active', (sender, isActive) => {
+        updateTab(tab.getId(), { active: isActive })
         if (isActive) {
           const tabValue = getTabValue(tabId)
           if (tabValue) {
