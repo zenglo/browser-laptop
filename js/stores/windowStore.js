a7db8a36413699a11591f6890b5c0d89f6645982
Pete Miller <miller.pete@gmail.com>
2018-02-20 17:57:50 -0800

    Add the single-webview, which only attaches to the active (or previewed) tab's guest, instead of rendering a `<webview />` for every open tab. Move active tab's non-webview (e.g. status bar, notifications, messagebox) components to `<Main />`, use `<GuestInstanceRenderer />` to display tab's and `WebviewDisplay` to manage webviews. Remove all webviews from <Frame /> component, subscribe to webContents events via WebContents and local emitter, instead of `<webview />` which was proxying events from WebContents inside muon.
    Create two implementations of WebviewDisplay: 1) pooled; and 2) true single webview.
    Pooled allows:
     - only hide the currently-displayed webview when the next webview has finished attaching and compositing, avoiding 'white flash' between tab switch.
     - effects between displaying different content, such as a fade (e.g. on preview) - not implemented, but would be simple css.
    Single webview allows simplified testing / debugging.
    Default to pooled as with C65, there is a big delay between detaching one tab's WebContents and attaching (and compositing) the next tab's WebContents.
    
    Force webview to paint on attach with workaround, avoiding C65 issues whereby the attached WebContents are not composited until either re-attach (achieved via `display: none` set and unset), or force re-paint with nudging css position.
    
    Pooled webview contains logic to abort an in-progress attach if a request to display another WebContents is received before the tab displays, and then continue with the new attach request. Since displaying a WebContents can take between 50ms and 250ms+, this avoids a flash or cycle between all the requested-to-be-displayed WebContents in the event that the user quickly cycles through tabs (or, more likely, previews).
    
    Pooled webview also must also harden itself against race conditions by checking WebContents is not destroyed before attach, and also is not destroyed during attach instead of attach completing succesfully.

--- js/stores/windowStore.js
+++ js/stores/windowStore.js
@@ -194,11 +194,10 @@ const frameGuestInstanceIdChanged = (state, action) => {
   action = makeImmutable(action)
   const oldGuestInstanceId = action.get('oldGuestInstanceId')
   const newGuestInstanceId = action.get('newGuestInstanceId')
-
   if (oldGuestInstanceId === newGuestInstanceId) {
     return state
   }
-
+  console.log('Frame guest instance id changed', { oldGuestInstanceId, newGuestInstanceId })
   return state.mergeIn(['frames', frameStateUtil.getFrameIndex(state, action.getIn(['frameProps', 'key']))], {
     guestInstanceId: newGuestInstanceId
   })
@@ -366,7 +365,7 @@ const doAction = (action) => {
         windowState = windowState.setIn(['ui', 'tabs', 'tabPageIndex'], action.index)
         windowState = windowState.deleteIn(['ui', 'tabs', 'previewTabPageIndex'])
       } else {
-        windowState = frameStateUtil.updateTabPageIndex(windowState, action.frameProps.get('tabId'))
+        windowState = frameStateUtil.updateTabPageIndex(windowState, action.tabId)
       }
       break
     case windowConstants.WINDOW_SET_TAB_HOVER_STATE:
