50208ce213aa30ded981b63e255e264b122c4acd
Cezar Augusto <cezaraugusto@outlook.it>
2018-02-22 16:12:12 -0300

    sync v2 - several UI changes user flow for sync improvements

--- app/sessionStore.js
+++ app/sessionStore.js
@@ -1087,7 +1087,8 @@ module.exports.defaultAppState = () => {
       lastFetchTimestamp: 0,
       objectsById: {},
       pendingRecords: {},
-      lastConfirmedRecordTimestamp: 0
+      lastConfirmedRecordTimestamp: 0,
+      setupCompleted: false
     },
     cache: {
       bookmarkLocation: undefined,
